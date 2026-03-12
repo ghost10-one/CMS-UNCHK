ddf3d575853fc8da6f931a1be8f3fdd0c7808d28
ghost10-one <azizsokhnamboup1o@gmail.com>
2026-03-12 00:32:07 +0000

    ajoute de wp -config

---
+++ wp-config.php
@@ -0,0 +1,15 @@
+<?php
+define('DB_NAME', 'cms_education');
+define('DB_USER', 'root');          // Windows : root, Linux : cms_user
+define('DB_PASSWORD', '');          // Windows : vide, Linux : motdepassefort
+define('DB_HOST', 'localhost');
+
+// Clés uniques de sécurité
+define('AUTH_KEY',         'groupe1');
+define('SECURE_AUTH_KEY',  'groupe1');
+define('LOGGED_IN_KEY',    'groupe1');
+define('NONCE_KEY',        'groupe1');
+define('AUTH_SALT',        'groupe1');
+define('SECURE_AUTH_SALT', 'groupe1');
+define('LOGGED_IN_SALT',   'groupe1');
+define('NONCE_SALT',       'groupe1');
